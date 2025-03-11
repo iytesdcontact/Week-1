bed37804cc4a2375fe1bcfa244d5acc4750da568
mehmetdurmaz <mehmet.durmaz@pia-team.com>
2025-03-12 00:29:37 +0300

    Init project

---
+++ src/main/java/com/softwaredevelopment2/kickoff/KickoffApplication.java
@@ -0,0 +1,13 @@
+package com.softwaredevelopment2.kickoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickoffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwaredevelopment2/kickoff/KickoffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwaredevelopment2.kickoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickoffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
